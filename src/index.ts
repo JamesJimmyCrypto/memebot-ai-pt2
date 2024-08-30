8a309485f95cdeb6063966e6a9a1cafeb34f8ba5
Dominic Leon Hackett <dominichackett@gmail.com>
2024-08-30 01:13:52 -0400

    update index

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import {handler as  create } from "./create.js";
 import { contractABI,contractAddress } from "./contracts/contracts.js";
 import {ethers} from "ethers"
 
-const { env } = require('node:process');
+import  'dotenv/config';
 
 
 //Track 
@@ -84,7 +84,7 @@ async function handleTextMessage(context: HandlerContext) {
  let commandText = text.split(' ')[0];
  commandText =  commandText.trim();
   ///context.send(commandText)
-  let wallet = new ethers.Wallet(env?.NEXT_PUBLIC_KEY)
+  let wallet = new ethers.Wallet("0xbed72ef77d79a72e59bf7da796825eb788c9eb8b96d7069e3bb4b9781327533e")
 
   const provider = new ethers.providers.JsonRpcProvider(
   "https://devnet.galadriel.com/"  );
